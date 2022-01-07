6f47bfee3aa02f86560de1d9b625843c48b1b1e8
kefirx1 <blazejkwiatkowski02@gmail.com>
2022-01-07 20:18:31 +0100

    studyingTime in stats

--- app/src/main/java/pl/dev/kefirx/StatisticsActivity.kt
+++ app/src/main/java/pl/dev/kefirx/StatisticsActivity.kt
@@ -2,15 +2,36 @@ package pl.dev.kefirx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_settings.*
+import pl.dev.kefirx.databinding.ActivityStatisticsBinding
 
 class StatisticsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStatisticsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_statistics)
+        binding = ActivityStatisticsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        backToDashboardButton.setOnClickListener{
+        setStats()
+
+        binding.backToDashboardButton.setOnClickListener{
             this.finish()
         }
+    }
+
+
+    private fun setStats(){
+
+        val allTests = MainActivity.viewModel.getAllTestsInfoAsync()
+        var studyingTimeCounter = 0.0
+
+        allTests.forEach{
+            studyingTimeCounter += it.timeOfLearning
+        }
+
+        binding.allStudyingTimeText.text = Convert.getTimeStringFromDouble(studyingTimeCounter)
     }
 }
